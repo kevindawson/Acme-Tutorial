870b407490c7f8e4b297bffcc7e607741b5580be
bowtie <bowtie@cpan.org>
2013-07-29 15:15:36 +0100

    nitial import

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Acme::Tutorial - Blah blah blah
+
+# SYNOPSIS
+
+    use Acme::Tutorial;
+
+# DESCRIPTION
+
+Acme::Tutorial is
+
+# AUTHOR
+
+Kevin Dawson <bowtie@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Kevin Dawson
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Acme/Tutorial.pm
@@ -0,0 +1,39 @@
+package Acme::Tutorial;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Tutorial - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Acme::Tutorial;
+
+=head1 DESCRIPTION
+
+Acme::Tutorial is
+
+=head1 AUTHOR
+
+Kevin Dawson E<lt>bowtie@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Kevin Dawson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
